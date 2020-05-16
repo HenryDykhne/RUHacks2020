8c13a11f354a7a884c5608affd850e4e10dea878
Henry Dykhne <edykhne@uoguelph.ca>
2020-05-16 03:03:49 -0400

    added early by time calc

--- utility.py
+++ utility.py
@@ -1,8 +1,25 @@
+import itertools
+from datetime import datetime, timedelta
 ##events passed to this function must be ordered by time
 def checkRoute(events):
     onTimeList = []
-    for i in my_list
-        print(i)
+    for first, second in itertools.izip(l, l[1:]): 
+        onTimeList.append(onTime(first, second, first.transportMode))
     return onTimeList
 
-def canBeOnTime(firstEvent, secondEvent):
+def onTime(firstEvent, secondEvent, transportMode):
+    transitTime = 13000##dummy. this is where transit time must be calculated
+    earlyBy = secondEvent.start.dateTime - (firstEvent.end.dateTime + timedelta(seconds=transitTime))
+    if earlyBy >= 0 
+        onTime = True
+    else
+        onTime = False
+
+    return {
+            u'onTime': onTime,
+            u'transitTime': transitTime,
+            u'earlyBy': earlyBy
+        }
+
+ev = [{"start":{"dateTime":}, ""}]
+print(checkRoute(ev)):
