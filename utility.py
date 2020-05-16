63f3f9bd2c7f3d420cc685a6fed797a18cf92035
Henry Dykhne <edykhne@uoguelph.ca>
2020-05-16 01:21:33 -0400

    bitofcodesaved

---
+++ utility.py
@@ -0,0 +1,8 @@
+##events passed to this function must be ordered by time
+def checkRoute(events):
+    onTimeList = []
+    for i in my_list
+        print(i)
+    return onTimeList
+
+def canBeOnTime(firstEvent, secondEvent):
