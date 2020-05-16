74192691328658302109cdde4ea958e9b5d2b15c
Henry Dykhne <edykhne@uoguelph.ca>
2020-05-16 16:31:30 -0400

    jquery calls now possible. file structure cleaned up

--- utility.py
+++ utility.py
@@ -1,4 +1,3 @@
-import itertools
 import distance
 import json
 from datetime import datetime, timedelta
@@ -6,7 +5,7 @@ from datetime import datetime, timedelta
 ##events passed to this function must be ordered by time
 def checkRoute(events):
     onTimeList = []
-    for firstEvent, secondEvent in itertools.izip(events, events[1:]): 
+    for firstEvent, secondEvent in zip(events, events[1:]): 
         onTimeList.append(onTime(firstEvent, secondEvent, firstEvent["transportMode"]))
 
     return json.dumps(onTimeList)
@@ -29,9 +28,4 @@ def onTime(firstEvent, secondEvent, transportMode):
         u'earlyBy': str(earlyBy)
     }
 
-ev = [
-    {"start":{"dateTime": datetime(2020, 12, 1, 1, 2, 3)}, "end":{"dateTime": datetime(2020, 12, 1, 4, 2, 3)}, "location": "toronto",  "transportMode": "driving"},
-    {"start":{"dateTime": datetime(2020, 12, 2, 6, 2, 3)}, "end":{"dateTime": datetime(2020, 12, 8, 1, 2, 3)}, "location": "florida",  "transportMode": "driving"},
-    {"start":{"dateTime": datetime(2020, 12, 3, 9, 2, 3)}, "end":{"dateTime": datetime(2020, 12, 3, 11, 2, 3)}, "location": "vaughan ontario",  "transportMode": "driving"}
-]
-print(checkRoute(ev))
+
