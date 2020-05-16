a9d1320ae6383dfa4fbb363b6a8623dd9ba0d56e
Cindy Pham <cindy619mysterio@gmail.com>
2020-05-16 14:23:32 -0500

    Fixed adding event feature on calendar

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,28 +1,37 @@
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
         plugins: ['timeGrid', 'dayGrid', 'interaction'],
         defaultView: 'timeGridWeek',
+        header: {
+            center: 'addEventButton'
+        },
+        customButtons: {
+            addEventButton: {
+                text: 'Add Event',
+                click: function() {
+                    $('#addModal').modal('show');
+                    $('#addButton').click(function () {
+                        var name = document.getElementById('eventName').value;
+                        var startDate = document.getElementById('startDate').value;
+                        var endDate = document.getElementById('endDate').value;
+                        calendar.addEvent({
+                            title: name,
+                            start: startDate,
+                            end: endDate
+                        });
+                    });
+                    document.getElementById('eventForm').reset();
+                }
+            }
+        },
         events: [
             {
                 title: 'Walmart Shopping',
                 start: '2020-05-16'
             }
-        ],
-        dateClick: function (info) {
-            $('#eventModal').modal('show');
-            $('#addButton').click(function () {
-                var name = document.getElementById('eventName').value;
-                var endDate = document.getElementById('endDate').value;
-                calendar.addEvent({
-                    title: name,
-                    start: info.dateStr,
-                    end: endDate
-                });
-            });
-            document.getElementById('eventForm').reset();
-        }
+        ]
     });
 
     calendar.render();
