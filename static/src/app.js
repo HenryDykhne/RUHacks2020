52732c4b902be0dc8f2d69f3d081ca5f0bcdefcd
Henry Dykhne <edykhne@uoguelph.ca>
2020-05-17 03:10:10 -0400

    set up connectin betweeen backend and frontend for route calculation

--- static/src/app.js
+++ static/src/app.js
@@ -1,23 +1,26 @@
 $("document").ready(function(){
 
-    $.ajax({
-        type: 'get',            //Request type
-        dataType: 'json',       //Data type - we will use JSON for almost everything 
-        url: '/test',   //The server endpoint we are connecting to
-        success: function (data) {
-            /*  Do something with returned object
-                Note that what we get is an object, not a string, 
-                so we do not need to parse it on the server.
-                JavaScript really does handle JSONs seamlessly
-            */
-            console.log(data); 
+    // $.ajax({
+    //     type: 'get',            //Request type
+    //     dataType: 'json',       //Data type - we will use JSON for almost everything 
+    //     url: '/test',   //The server endpoint we are connecting to
+    //     success: function (data) {
+    //         /*  Do something with returned object
+    //             Note that what we get is an object, not a string, 
+    //             so we do not need to parse it on the server.
+    //             JavaScript really does handle JSONs seamlessly
+    //         */
+    //         console.log(data); 
 
-        },
-        fail: function(error) {
-            // Non-200 return, do something with error
-            console.log(error); 
-        }
-    });
+    //     },
+    //     fail: function(error) {
+    //         // Non-200 return, do something with error
+    //         console.log(error); 
+    //     }
+    // });
+    
+
+    
 
     function getEvents(id){
         $.ajax({
@@ -27,7 +30,7 @@ $("document").ready(function(){
             data: {'userID':id},
             success: function (data) {
                 console.log(data); 
-                return data;
+                //add to page
             },
             fail: function(error) {
                 // Non-200 return, do something with error
@@ -36,6 +39,30 @@ $("document").ready(function(){
         });
     }
 
-    console.log(getEvents("userID"));
+    function getOnTimeList(events){
+        $.ajax({
+            type: 'POST',           //Request type
+            dataType: 'json',      //Data type - we will use JSON for almost everything 
+            url: '/getOnTimeList',   //The server endpoint we are connecting to
+            data: {'events':events},
+            success: function (data) {
+                console.log(data); 
+                //add to page
+            },
+            fail: function(error) {
+                // Non-200 return, do something with error
+                console.log(error); 
+            }
+        });
+    }
+
+    ev = [
+        {"start":{"dateTime": new Date()}, "end":{"dateTime": new Date()}, "location": "toronto",  "transportMode": "driving"},
+        {"start":{"dateTime": new Date()}, "end":{"dateTime": new Date()}, "location": "florida",  "transportMode": "driving"},
+        {"start":{"dateTime": new Date()}, "end":{"dateTime": new Date()}, "location": "vaughan ontario",  "transportMode": "driving"}
+    ]
+    
+    JSON.stringify(getOnTimeList(JSON.stringify(ev)));
+
 });
 
